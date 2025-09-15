f0e069079b4e36dc9f32f2436362968df444e1f1
Haze_of_dream <74027566+Hazeofdream@users.noreply.github.com>
2025-09-14 22:01:50 -0500

    fix

--- godmmode-enforcer/lua/autorun/cl_godmode.lua
+++ godmmode-enforcer/lua/autorun/cl_godmode.lua
@@ -1,8 +1,8 @@
 if not CLIENT then return end
 
 -- Branding Options
-local GodmodeCategory = "Excelsus"
-local GodmodeSubName = "Nametags"
+local PanelCategory = "Excelsus"
+local PanelName = "Nametags"
 
 local settingsFile = "godmode_enforcer/settings.json"
 local settings = {}
@@ -159,7 +159,7 @@ end)
 
 -- ===== Options menu =====
 hook.Add("PopulateToolMenu", "GodmodeEnforcer_OptionsMenu", function()
-    spawnmenu.AddToolMenuOption("Options", GodmodeCategory, GodmodeSubName, "GodmodePanel", "", "", function(panel)
+    spawnmenu.AddToolMenuOption("Options", PanelCategory, "GodmodePanel", PanelName, "", "", function(panel)
         panel:Clear()
 
         local enable = vgui.Create("DCheckBoxLabel", panel)
